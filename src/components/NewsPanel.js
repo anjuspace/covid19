eed93ea11990c95a64bea2e9862addcf64a054bf
AnjuSpace <anjuglobal2017@gmail.com>
2020-03-23 16:30:09 +0000

    merge changes from upstream

---
+++ src/components/NewsPanel.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+import i18n from '../data/i18n.yml'
+
+export default class NewsPanel extends Component {
+    render() {
+        const { data, currentRegion, date, lang, fullPlot } = this.props
+        return <div >Hello World</div>
+    }
+}
