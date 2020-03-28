ddc1eacc757905d84fbda5accce32c92933f8d3a
leizhaoanju <leizhao.ld@gmail.com>
2020-03-28 09:45:01 +0000

    update data

--- src/scripts/data_processing_apinews.js
+++ src/scripts/data_processing_apinews.js
@@ -5,7 +5,7 @@ const fetch = require('node-fetch')
 const fs = require('fs')
 
 var today = new Date();
-var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+(today.getDate()-5);
+var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+(today.getDate()-1);
 
  var pairs2 = {
   "items": [
